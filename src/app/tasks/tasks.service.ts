625922c24898443216be044bebb3ce469f602008
Richard Burman <richard.burman@harperjames.co.uk>
2025-02-21 14:28:10 +0000

    feat: integrate LoggingService into TasksService for enhanced task management logging

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,24 +1,33 @@
-import { Injectable, signal } from "@angular/core";
-import { Task, TaskStatus } from "./task.model";
+import { inject, Injectable, signal } from '@angular/core';
+
+import { Task, TaskStatus } from './task.model';
+import { LoggingService } from '../logging.service';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class TasksService {
-    private tasks = signal<Task[]>([]);
+  private tasks = signal<Task[]>([]);
+  private loggingService = inject(LoggingService);
 
-    allTasks = this.tasks.asReadonly();
+  allTasks = this.tasks.asReadonly();
 
-    addTask(taskData: {title: string; description: string }) {
-        const newTask: Task = {
-            ...taskData,
-            id: Math.random().toString(),
-            status: 'OPEN'
-        };
-        this.tasks.update((oldTasks) => [...oldTasks, newTask]); 
-    }
+  addTask(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN',
+    };
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+    this.loggingService.log('ADDED TASK WITH TITLE ' + taskData.title);
+  }
 
-    updateTasksStatus(taskId: string, newStatus: TaskStatus) {
-        this.tasks.update((oldTasks) => oldTasks.map((task) =>  task.id === taskId ? {...task, status: newStatus} : task));
-    }
+  updateTasksStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
+    this.loggingService.log('UPDATED TASK STATUS TO ' + newStatus);
+  }
 }
