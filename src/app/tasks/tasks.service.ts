c0293d65706fb6a90b5c94335452433e6355b6e2
Richard Burman <richard.burman@harperjames.co.uk>
2025-02-21 14:11:26 +0000

    refactor: remove TasksService provider from TasksComponent and enable root-level injection in TasksService

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, signal } from "@angular/core";
 import { Task, TaskStatus } from "./task.model";
 
-// @Injectable({
-//     providedIn: 'root'
-// })
+@Injectable({
+    providedIn: 'root'
+})
 export class TasksService {
     private tasks = signal<Task[]>([]);
 
@@ -20,6 +20,5 @@ export class TasksService {
 
     updateTasksStatus(taskId: string, newStatus: TaskStatus) {
         this.tasks.update((oldTasks) => oldTasks.map((task) =>  task.id === taskId ? {...task, status: newStatus} : task));
-
     }
 }
